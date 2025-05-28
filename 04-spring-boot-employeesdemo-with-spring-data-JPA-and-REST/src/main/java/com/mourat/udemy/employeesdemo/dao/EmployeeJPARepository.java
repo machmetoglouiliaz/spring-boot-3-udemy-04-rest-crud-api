18da25c7108789b1f11039a8900d17620bb7078b
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-28 17:10:23 +0300

    added experiment with Spring Data REST configs

--- 04-spring-boot-employeesdemo-with-spring-data-JPA-and-REST/src/main/java/com/mourat/udemy/employeesdemo/dao/EmployeeJPARepository.java
+++ 04-spring-boot-employeesdemo-with-spring-data-JPA-and-REST/src/main/java/com/mourat/udemy/employeesdemo/dao/EmployeeJPARepository.java
@@ -2,6 +2,9 @@ package com.mourat.udemy.employeesdemo.dao;
 
 import com.mourat.udemy.employeesdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path="members")
 
 public interface EmployeeJPARepository extends JpaRepository<Employee, Integer> {
 }
